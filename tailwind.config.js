65e98c6ef5e6d6768ec5f0c21d6963ea543331b3
pasidu10 <punnyanamda.thero@gmail.com>
2025-11-11 19:55:26 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        pasiyaBlue: "#007AFF",
+        pasiyaGreen: "#1ED760",
+        pasiyaWhite: "#F9FAFB"
+      }
+    },
+  },
+  plugins: [],
+}
