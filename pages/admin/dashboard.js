ca4b89ee299ea731f45b389cdc401d73f9564b82
pasidu10 <punnyanamda.thero@gmail.com>
2025-11-11 21:55:30 +0530

    Update dashboard.js

--- pages/admin/dashboard.js
+++ pages/admin/dashboard.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { auth } from "@/firebaseConfig";
+import { auth } from "../../firebaseConfig";
 import { onAuthStateChanged, signOut } from "firebase/auth";
 import { useRouter } from "next/router";
 
