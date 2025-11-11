9ef44ae5a22f7b1f7373429756dfec5a05e3f76a
pasidu10 <punnyanamda.thero@gmail.com>
2025-11-11 21:37:25 +0530

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,21 +1,18 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBaVnGVgNBLYWLZZFWh4klvqowrk5tDqXc",
-  authDomain: "pasiya-ai.firebaseapp.com",
-  projectId: "pasiya-ai",
-  storageBucket: "pasiya-ai.firebasestorage.app",
-  messagingSenderId: "51322829115",
-  appId: "1:51322829115:web:c411a7303b0644df4edf1e",
-  measurementId: "G-Z62NGWK4M0"
+    apiKey: "AIzaSyBaVnGVgNBLYWLZZFWh4klvqowrk5tDqXc",
+    authDomain: "pasiya-ai.firebaseapp.com",
+    projectId: "pasiya-ai",
+    storageBucket: "pasiya-ai.firebasestorage.app",
+    messagingSenderId: "51322829115",
+    appId: "1:51322829115:web:bec65a5c4858bbea4edf1e",
+    measurementId: "G-L7RQQGRFXS"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
+
